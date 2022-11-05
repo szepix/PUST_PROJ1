3f9a7ec2e9f0240289ff47a8e96cc9ebcb44c8b0
Adam <01158187@pw.edu.pl>
2022-11-05 17:39:44 +0100

    Zadanias

--- Zad_1.m
+++ Zad_1.m
@@ -3,7 +3,7 @@ clear all
 %% Inicjalizacja zmienych
 
 T = 0.5; %Czas próbkowania
-t_sym = 200; %Czas symulacji (w sekundach)
+t_sym = 50; %Czas symulacji (w sekundach)
 
 kp = 12; %Krok początkowy startu symulacji
 kk = t_sym/T; %Krok końcowy
@@ -14,16 +14,22 @@ Ypp = 4;
 Umin = 0.3;
 Umax = 0.7;
 
-U(1:kp,1) = Upp;
-Y(1:kp,1) = Ypp;
+U(1:kp,1) = 0;
+Y(1:kp,1) = 0;
 
 
 %% Symulacja obiektu
 
 for k = kp:kk
     Y(k) = symulacja_obiektu1Y_p1(U(k-10),U(k-11),Y(k-1),Y(k-2));
-    U(k) = Upp+0.1;
+    U(k) = Upp;
 end
 
-%% Plot 
-plot(1:kk,Y)
+%% Plot
+set(0,'defaultLineLineWidth',1);
+stairs(1:kk,Y)
+hold on
+stairs(1:kk,U,"--")
+hold off
+legend("y(k)","u(k)")
+exportgraphics(gca,'y_p_p.pdf')
---
+++ Zad_2_a.m
@@ -0,0 +1,52 @@
+clear all
+
+%% Inicjalizacja zmienych
+
+T = 0.5; %Czas próbkowania
+t_sym = 250; %Czas symulacji (w sekundach)
+
+kp = 12; %Krok początkowy startu symulacji
+kk = t_sym/T; %Krok końcowy
+
+Upp = 0.5;
+Ypp = 4;
+
+Umin = 0.3;
+Umax = 0.7;
+
+
+U(1:kp,1) = 0.5;
+Y(1:kp,1) = 4;
+
+Uzad(1:100) = 0.5;
+Uzad(100:200) = 0.6;
+Uzad(200:300) = 0.4;
+Uzad(300:400) = 0.7;
+Uzad(400:kk) = 0.3;
+
+%% Symulacja obiektu
+
+for k = kp:kk
+    Y(k) = symulacja_obiektu1Y_p1(U(k-10),U(k-11),Y(k-1),Y(k-2));
+    U(k) = Uzad(k);
+end
+
+%% Plot
+set(0,'defaultLineLineWidth',1);
+figure
+subplot(2,1,1)
+stairs(1:kk,Y)
+xlabel("k")
+ylabel("y")
+set(get(gca,'ylabel'),'rotation',0)
+title("Y od k")
+ylim([3.4, 4.6])
+
+subplot(2,1,2)
+stairs(1:kk,U)
+xlabel("k")
+ylabel("u")
+set(get(gca,'ylabel'),'rotation',0)
+title("U od k")
+ylim([0.2, 0.8])
+exportgraphics(gca,'odp_skok.pdf')
